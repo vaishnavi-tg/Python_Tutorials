4d9ce5e89c57b6807d5ce5d18251d48ceb2318e2
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-02-10 17:45:12 +0530

    Added few files

---
+++ Sparrow_coding_labs_python/Areaofcircle.py
@@ -0,0 +1,8 @@
+#Write a Python program that calculates the area of a circle.
+#Prompt the user to enter the radius, and then compute and display the 
+#area. Use the formula: Area = π * r^2, where π (pi) is approximately 3.14159
+import math 
+radius=float(input("Enter the radius\n"))
+area=math.pi*radius**2
+   
+
---
+++ Sparrow_coding_labs_python/Areaofrectangle.py
@@ -0,0 +1,8 @@
+#Write a Python program that takes the length and width of a rectangle as 
+#input from the user and calculates its area. Display the result as "The area
+# of the rectangle is X square units," where X is the calculated area.
+
+len=int(input("Enter the length\n"))
+wid=int(input("Enter the width\n"))
+area=len*wid
+print("The area of the rectangle is ", area,"square units")
