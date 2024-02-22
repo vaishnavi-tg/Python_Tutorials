c29edf34280dfcfe57d74d39444e35a04cb99937
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-02-22 17:56:09 +0530

    Added few files

---
+++ PW_Python/Multithreading.py
@@ -0,0 +1,10 @@
+import threading
+def test(id):
+    print("This is my test id",id)
+
+test(9)
+test(4)
+test(2)    
+
+
+
---
+++ PW_Python/Oops.py
@@ -0,0 +1,12 @@
+class test:
+    pass
+a=test()
+print(type(a))
+class pwskills:
+    def welcome_msg():
+        print("Hello  !! Welcome to pw skills")
+
+vaishnavi=pwskills()
+vaishnavi.welcome_msg        
+
+
