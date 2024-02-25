48d239eab229cd5624fc9f95fb875c5a8c75db55
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-02-25 18:58:02 +0530

    Added few files

---
+++ Harry Python/Conditional.py
@@ -0,0 +1,15 @@
+'''a=40
+if(a>5):
+    print("a is greater than 5")
+elif(a<5):
+    print("a is less than 5")
+else:
+   print("a is equal to 5")'''
+
+
+
+age=int(input("enter the age of the person      "))
+if(age>=18):
+    print("Age is greater than 18 and the age is",age)
+else:
+    print("Age is less than 18 and the age is",age)
