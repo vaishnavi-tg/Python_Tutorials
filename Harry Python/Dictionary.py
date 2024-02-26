eaf31b49d546c511c7f6d5bb57fca5d3d8c944a8
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-02-26 18:58:45 +0530

    Added few files

---
+++ Harry Python/Dictionary.py
@@ -0,0 +1,8 @@
+# Its a collection of key values pairs
+oxford={"gift":"The present given to sumone with will",
+        "on":"its a logial operator in programming"}
+print(oxford)
+print(type(oxford))
+print(oxford["on"])
+print(oxford.items)
+print()
