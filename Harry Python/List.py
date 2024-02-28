fcebee1ba83fab7a18ef76487aceb696fe0feb17
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-02-28 19:00:12 +0530

    Added few files

---
+++ Harry Python/List.py
@@ -0,0 +1,48 @@
+#List and slicing
+'''l=[1,'vaish',False,(1,2),[1,2],None,12.3,'s',1234]
+print(type(l))
+print(l[2])
+print(l[2:6])
+print(l[2:10])'''
+
+
+#List methods
+'''l1=[1,2,3,5,4,6,4,6,8,9]
+print(l1)
+l1.sort() #sorts the original list
+print(l1)'''
+
+
+'''l1=[1,2,3,5,4,6,4,6,8,9]
+print(l1)
+l1.reverse() #reverses the original list
+print(l1)'''
+
+
+'''l1=[1,2,3,5,4,6,4,6,8,9]
+l1.append(6)#appends at the end of the list
+print(l1)'''
+
+
+'''l1=[1,2,3,5,4,6,4,6,8,9]
+l1.insert(6,7)#inserts at specific position
+print(l1)'''
+
+
+'''l1=[1,2,3,5,4,6,4,6,8,9]
+l1.insert(6,7)#inserts at specific position
+print(l1)'''
+
+
+'''l1=[1,2,3,5,4,6,4,6,8,9]
+l1.pop()
+l1.pop()
+l1.pop()#delete element at the index and returns the value 
+print(l1)'''
+
+
+l1=[1,2,3,5,4,6,4,6,8,9]
+l1.remove(4)
+l1.remove(5)#removes the specified  number 
+print(l1)
+
