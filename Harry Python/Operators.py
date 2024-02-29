97b633e89699d3d7007f4c94fd52f75d779e999f
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-02-29 19:01:00 +0530

    Added few files

---
+++ Harry Python/Operators.py
@@ -0,0 +1,19 @@
+a=13
+b=5
+#Arithematic operators
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#assignment operator
+c=5
+c+=5
+d=15
+d-=3
+print(c,d)
+#demonstrating logical operator in python 
+e=6
+f=5
+print(e==f and e>f)
+print(e==f or e>f)
+print(not(e>f))
