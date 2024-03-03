0e213c2705d6001d60c35e61de8953ca7f38fd1b
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-03-03 19:02:37 +0530

    Added few files

---
+++ Harry Python/typecasting.py
@@ -0,0 +1,33 @@
+a = 56
+b = '56'
+c = int(b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(a == c)
+
+print(b == c)
+print(a)
+print(b)
+print(c)
+
+
+# type()
+a = input("enter your name \n")
+print("The entered name is ", a)
+
+
+a = int(input("enter the fisrt number "))
+b = int(input("enter the second number "))
+print("the sum of a and b is ", a+b)
+
+
+
+a = input("enter the fisrt number ")
+b = input("enter the second number ")
+print("the sum of a and b is ", int(a)+int(b))
+
+
+
+a=input("56")
+print(type(a))
