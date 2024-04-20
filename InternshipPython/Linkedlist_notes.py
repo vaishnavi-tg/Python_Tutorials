851e11dc64ce885b10f85ca6a1c723470234b552
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-04-20 19:39:58 +0530

    Added few files

---
+++ InternshipPython/Linkedlist_notes.py
@@ -0,0 +1,20 @@
+''' A linked list can have its elements to be dynmically allocated
+Linear Linked list or One -Way list or singly Linked list 
+Double Linked list or Two-Way List
+Circular Linked list
+# Advantages 
+Dynamic in nature 
+Insertion and deletion are easy 
+Memory Utilization 
+
+
+
+#Disadvantages 
+Wastage of memory 
+No random access
+Time complexitty 
+Reverse traversing is difficult 
+Heap space restriction 
+
+
+'''
