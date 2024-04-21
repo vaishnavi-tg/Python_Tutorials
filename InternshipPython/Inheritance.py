95dca5abeb9af610d6eb2435f7ef3a21a0594241
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-04-21 19:39:08 +0530

    Added few files

---
+++ InternshipPython/Inheritance.py
@@ -0,0 +1,32 @@
+class grandpa:
+  n=1000000000
+class father(grandpa):
+  m=10000000
+class you:
+  k=10000 
+
+f=father()
+print(f.n)
+y=you()
+print(y.k)
+
+
+# Multi Level Inheritance
+class kaidhi:
+  s1="Dilli"
+  s2="Bejoy"
+class vikram(kaidhi):
+  s1="Vikram"
+  s2="Sandhanam"
+  s3="Amar"
+  s4="Rolex"
+class leo(vikram):
+  s="Leo Dass"
+  def parthi(self):
+    print("Bloody Sweat")
+l=leo()
+print(l.s1)      
+
+# Mutiple Inheritance 
+
+
