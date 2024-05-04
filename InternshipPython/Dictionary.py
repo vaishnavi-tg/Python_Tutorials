9520804ceac6ee8c2a11c236b0f1fa95783f1fb5
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-05-04 19:24:47 +0530

    Added few files

---
+++ InternshipPython/Dictionary.py
@@ -0,0 +1,23 @@
+d={'v':'vaishnavi','s':5}
+print(type(d))
+
+
+
+# V can create dictonariees of dictionary 
+car1={'Model':'Maruthi Suzuki','Price':'7000000','Mileage':'10kmh'}
+car2={'Model':'Toyota Innova','Price':'8000000','Mileage':'11kmh'}
+car3={'Model':'Scorpio','Price':'9000000','Mileage':'12kmh'}
+print(car1)
+print(car2)
+print(car3)
+print(car2["Price"])
+# V ca give different keys even type of keys also
+
+
+
+m1={'movie':str(input("Enter the Movie name\n")),'Ratings':int(input("Enter the ratings\n"))}
+print(m1)
+print(m1["movie"])
+print(m1["Ratings"])
+
+
