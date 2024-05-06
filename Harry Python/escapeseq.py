ad1083c0f826d21f469a490babed1801eddc0be7
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-05-06 19:05:20 +0530

    Added few files

---
+++ Harry Python/escapeseq.py
@@ -0,0 +1,9 @@
+mystr="This is the reconstructed newyork city"
+print(mystr)
+print("This is the reconstructed \n newyork city")
+print("This is the reconstructed \t newyork city")
+print("This is the reconstructed \\ newyork city")
+print("This is the reconstructed \' newyork city") 
+print("This is the reconstructed '' newyork city") 
+print("This is the reconstructed \" newyork city")
+print('This is the reconstructed \' newyork city')
