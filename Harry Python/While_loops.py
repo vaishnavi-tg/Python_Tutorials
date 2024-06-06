849e016533fc477f252063c42890f7431ce7997f
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-06-06 19:06:23 +0530

    Added few files

---
+++ Harry Python/While_loops.py
@@ -0,0 +1,37 @@
+'''num=1
+while(num<=100):
+ print(num)
+ num+=1'''
+
+
+'''i=1
+while(i<=20):
+ i+=1
+ print(i)'''
+
+
+
+#  quiz
+'''a=0
+while(a<=50):
+ print(a)
+ a+=1'''
+
+
+
+
+'''i=0
+while(i<=4):
+    print("sorry")
+    i=i+1'''
+
+
+
+
+list=[1,2,3,4,'chinns','23.567',45,"The chandrayan 3"]
+i=0
+while(i<len(list)):
+    print(list[i])
+    i+=1
+
+
