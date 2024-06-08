8099d04f33c198cdcaa36e05a3c5f96a99f4b5f8
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-06-08 19:10:03 +0530

    Added few files

---
+++ Harry Python/String.py
@@ -0,0 +1,12 @@
+name='Jeff be"sos'
+name2="Adam D'angelo"# quora founder 
+name3='''Twinkle twinkle little star 
+how i wonder' wat u r 
+up above the world" so high 
+like a diamond in the sky'''
+print(name)
+print(type(name))
+print(name2)
+print(type(name2))
+print(name3)
+print(type(name3))
---
+++ Harry Python/Stringfunctions.py
@@ -0,0 +1,17 @@
+mystr="abcdefghijklmnopqrstuvwxyzd"
+print(len(mystr))
+print(mystr.endswith('lly'))
+print(mystr.endswith('xyz'))
+print(mystr.startswith('xyz'))
+print(mystr.startswith('abc'))
+print(mystr.count('d',))
+myStr="my name is vaishnavi"
+print(myStr.capitalize())
+print(myStr.find('n'))
+print(myStr.replace("name","self"))
+
+
+
+
+
+
