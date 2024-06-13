65cb4ce1892272e5360cdad2d6e847e5ecf44d72
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-06-13 19:17:35 +0530

    Added few files

---
+++ Harry Python/Tuples.py
@@ -0,0 +1,6 @@
+t=(1,4,5,6)
+t1=(2,)#if tuple has single element end it with a comma
+print(t)
+print(t1)
+print(type(t))
+print(type(t1))
---
+++ Harry Python/Type().py
@@ -0,0 +1,10 @@
+a="this is tom"
+b=56
+c=None
+d=False
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
